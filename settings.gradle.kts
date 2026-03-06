f740a31f1399e45bd6b884dea057937261843866
._______166 <retromac9x2@gmail.com>
2026-03-06 13:35:23 +0000

    gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { setUrl("../../../prebuilts/fullsdk-darwin/extras/android/m2repository") }
+        maven { setUrl("../../../prebuilts/fullsdk-linux/extras/android/m2repository") }
+        mavenCentral()
+        google()
+        jcenter()
+    }
+}
+rootProject.name = "LatinIME"
