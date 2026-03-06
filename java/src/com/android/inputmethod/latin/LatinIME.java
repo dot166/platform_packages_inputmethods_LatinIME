09440f981002ecee13d91d611ee45e474e02a309
._______166 <retromac9x2@gmail.com>
2026-03-06 09:44:15 +0000

    gradle

--- java/src/com/android/inputmethod/latin/LatinIME.java
+++ java/src/com/android/inputmethod/latin/LatinIME.java
@@ -136,13 +136,13 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
      * A broadcast intent action to hide the software keyboard.
      */
     static final String ACTION_HIDE_SOFT_INPUT =
-            "com.android.inputmethod.latin.HIDE_SOFT_INPUT";
+            "com.android.inputmethod.latin.HIDE_SOFT_INPUT2";
 
     /**
      * A custom permission for external apps to send {@link #ACTION_HIDE_SOFT_INPUT}.
      */
     static final String PERMISSION_HIDE_SOFT_INPUT =
-            "com.android.inputmethod.latin.HIDE_SOFT_INPUT";
+            "com.android.inputmethod.latin.HIDE_SOFT_INPUT2";
 
     /**
      * The name of the scheme used by the Package Manager to warn of a new package installation,
