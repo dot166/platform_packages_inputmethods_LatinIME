a8900ca454a2db1f9034e86e3353f8405e957b88
._______166 <retromac9x2@gmail.com>
2026-06-01 22:22:06 +0100

    misc dict stuff

--- java/src/com/android/inputmethod/latin/utils/DictionaryInfoUtils.java
+++ java/src/com/android/inputmethod/latin/utils/DictionaryInfoUtils.java
@@ -397,9 +397,6 @@ public class DictionaryInfoUtils {
      * are for, and this method returns this ID.
      */
     public static String getMainDictId(@Nonnull final Locale locale) {
-        if (locale.getISO3Language().equals("jpn")) {
-            return "mozc.data"; // this won't ever change, mozc will always point to mozc.data
-        }
         // This works because we don't include by default different dictionaries for
         // different countries. This actually needs to return the id that we would
         // like to use for word lists included in resources, and the following is okay.
